8dec10cdff9aad48334695b003bbda5ea70808c9
sebastiannrodriguezz009-dotcom <sebastiannrodriguezz009@gmail.com>
2026-04-15 13:40:41 -0500

    seagregocoment

--- main.java
+++ main.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 //de aqui para abajo esta la magia
 //chao
-=======
 //de aqui para abajo esta la magia hola kylian mbape
->>>>>>> 09f0c29f697cdd1f8e472d8b221bfad32bb3f7bf
 //q extraño
